fa76c0f4d0641c2dd1013cffd566eb08420b0f46
sohamahire <66758574+sohamahire@users.noreply.github.com>
2020-12-18 19:13:32 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# VIRTUAL-PET-1
+# Project34
---
+++ sketch.js
@@ -0,0 +1,121 @@
+//Create variables here
+var database;
+var dog,dogImage,dogImage1,food,foodImage,foodStock,foodRef;
+
+function preload()
+{
+  //load images here
+  backgroundImg = loadImage("images/bg.png");
+  dogImage = loadImage("images/Dog.png");
+  dogImage1 = loadImage("images/happydog.png");
+  foodImage = loadImage("images/Bone.png");
+  bathImage = loadImage("images/bath.png");
+  sleepImage = loadImage("images/sleep.png");
+  playImage = loadImage("images/play.png");
+  walkImage = loadImage("images/walk.png");
+  
+
+}
+
+function setup() {
+  createCanvas(480, 480);
+
+  //Sprites
+
+  food = createSprite(250,400,50,50);
+  food.addImage(foodImage);
+  food.scale = 0.3;
+
+
+  dog = createSprite(400,150);
+  dog.addImage(dogImage);
+  dog.scale = 0.2;
+
+  //Firebase
+  database = firebase.database();
+
+  //Reference for food
+  foodRef = database.ref("Food");
+  foodRef.on("value",read,console.log("error"));
+
+  foodRef.set(20);
+
+
+}
+
+
+function draw() {  
+  background(backgroundImg);
+  drawSprites();
+  
+  //add styles here
+  textSize(32);
+  fill("blue");
+  text("Bones in the Stock: "+foodStock,50,300);
+ // textSize(25);
+  //text("Hi! Will you help me in doing some works ?",50,70)
+  decreaseFood();
+  if(foodStock===0){
+    foodStock = 20;
+  }
+
+  if(keyWentUp(DOWN_ARROW)){
+    
+    dog.addImage(bathImage);
+    
+    
+  }
+
+  if(keyWentUp(LEFT_ARROW)){
+   
+    dog.addImage(sleepImage);
+    dog.scale = 0.3
+    
+    
+  }
+
+  if(keyWentUp(RIGHT_ARROW)){
+   
+    dog.addImage(playImage);
+    dog.scale = 0.3
+    
+    
+  }
+
+  if(keyCode === 32){
+    
+    dog.addImage(walkImage);
+    dog.scale = 0.3
+ }
+
+}
+
+function read(data){
+  foodStock = data.val();
+}
+
+function decreaseFood(){
+  if(keyWentDown(UP_ARROW)){
+  foodRef = database.ref("Food");
+  foodStock = foodStock - 1;
+  foodRef.set(foodStock);
+  dog.addImage(dogImage1);
+  food.x = 350;
+  food.y = 200;
+  food.scale = 0.1;
+
+  }
+  
+  if(keyWentUp(UP_ARROW)){
+    
+    foodStock = foodStock;
+    dog.addImage(dogImage);
+    fill("yellow");
+    text('Thank you 🥳🥳',10,80);
+    food.x = 250;
+    food.y = 400;
+    food.scale = 0.2;
+    
+  }
+}
+
